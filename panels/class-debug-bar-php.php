86fe39b37506900bbc2392e3ca2221069bf2fd72
Steven A. Zahm <shazahm1@hotmail.com>
2014-05-09 14:44:40 -0400

    Add support for STRICT and DEPRECATED PHP notices.

--- panels/class-debug-bar-php.php
+++ panels/class-debug-bar-php.php
@@ -3,6 +3,9 @@
 class Debug_Bar_PHP extends Debug_Bar_Panel {
 	var $warnings = array();
 	var $notices = array();
+	var $strict = array();
+	var $deprecated = array();
+	var $unknown = array();
 	var $real_error_handler = array();
 
 	function init() {
@@ -15,7 +18,7 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 	}
 
 	function is_visible() {
-		return count( $this->notices ) || count( $this->warnings );
+		return count( $this->notices ) || count( $this->warnings ) || count( $this->strict ) || count( $this->deprecated ) || count( $this->unknown );
 	}
 
 	function debug_bar_classes( $classes ) {
@@ -23,30 +26,41 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 			$classes[] = 'debug-bar-php-warning-summary';
 		elseif ( count( $this->notices ) )
 			$classes[] = 'debug-bar-php-notice-summary';
+		elseif ( count( $this->strict ) )
+			$classes[] = 'debug-bar-php-notice-summary';
+		elseif ( count( $this->deprecated ) )
+			$classes[] = 'debug-bar-php-notice-summary';
+		elseif ( count( $this->unknown ) )
+			$classes[] = 'debug-bar-php-notice-summary';
 		return $classes;
 	}
 
 	function error_handler( $type, $message, $file, $line ) {
 		$_key = md5( $file . ':' . $line . ':' . $message );
 
+		$count = array_key_exists( $_key, $this->strict ) ? $this->strict[$_key]['count'] : 0;
+		$count++;
+
 		switch ( $type ) {
 			case E_WARNING :
 			case E_USER_WARNING :
-				$this->warnings[$_key] = array( $file.':'.$line, $message );
+				$this->warnings[$_key] = array( $file.':'.$line, $message, 'count' => $count );
 				break;
 			case E_NOTICE :
 			case E_USER_NOTICE :
-				$this->notices[$_key] = array( $file.':'.$line, $message );
+				$this->notices[$_key] = array( $file.':'.$line, $message, 'count' => $count );
 				break;
+			case 2048:
 			case E_STRICT :
-				// TODO
+				$this->strict[$_key] = array( $file.':'.$line, $message, 'count' => $count );
 				break;
+			case 8192:
 			case E_DEPRECATED :
 			case E_USER_DEPRECATED :
-				// TODO
+				$this->deprecated[$_key] = array( $file.':'.$line, $message, 'count' => $count );
 				break;
 			case 0 :
-				// TODO
+				$this->unknown[$_key] = array( $file.':'.$line, $message, 'count' => $count );
 				break;
 		}
 
@@ -60,19 +74,46 @@ class Debug_Bar_PHP extends Debug_Bar_Panel {
 		echo "<div id='debug-bar-php'>";
 		echo '<h2><span>Total Warnings:</span>' . number_format( count( $this->warnings ) ) . "</h2>\n";
 		echo '<h2><span>Total Notices:</span>' . number_format( count( $this->notices ) ) . "</h2>\n";
+		echo '<h2><span>Total Strict:</span>' . number_format( count( $this->strict ) ) . "</h2>\n";
+		echo '<h2><span>Total Deprecated:</span>' . number_format( count( $this->deprecated ) ) . "</h2>\n";
+		echo '<h2><span>Total Unknown:</span>' . number_format( count( $this->unknown ) ) . "</h2>\n";
 		if ( count( $this->warnings ) ) {
 			echo '<ol class="debug-bar-php-list">';
 			foreach ( $this->warnings as $location_message) {
-				list( $location, $message) = $location_message;
-				echo "<li class='debug-bar-php-warning'>WARNING: ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+				list( $location, $message, $count ) = array_values( $location_message );
+				echo "<li class='debug-bar-php-warning'>WARNING (" . $count . ") : ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
 			}
 			echo '</ol>';
 		}
 		if ( count( $this->notices ) ) {
 			echo '<ol class="debug-bar-php-list">';
 			foreach ( $this->notices as $location_message) {
-				list( $location, $message) = $location_message;
-				echo "<li  class='debug-bar-php-notice'>NOTICE: ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+				list( $location, $message, $count ) = array_values( $location_message );
+				echo "<li  class='debug-bar-php-notice'>NOTICE (" . $count . ") : ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+			}
+			echo '</ol>';
+		}
+		if ( count( $this->strict ) ) {
+			echo '<ol class="debug-bar-php-list">';
+			foreach ( $this->strict as $location_message) {
+				list( $location, $message, $count ) = array_values( $location_message );
+				echo "<li  class='debug-bar-php-notice'>STRICT (" . $count . ") : ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+			}
+			echo '</ol>';
+		}
+		if ( count( $this->deprecated ) ) {
+			echo '<ol class="debug-bar-php-list">';
+			foreach ( $this->deprecated as $location_message) {
+				list( $location, $message, $count ) = array_values( $location_message );
+				echo "<li  class='debug-bar-php-notice'>DEPRECATED (" . $count . ") : ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
+			}
+			echo '</ol>';
+		}
+		if ( count( $this->unknown ) ) {
+			echo '<ol class="debug-bar-php-list">';
+			foreach ( $this->unknown as $location_message) {
+				list( $location, $message, $count ) = array_values( $location_message );
+				echo "<li  class='debug-bar-php-notice'>UNKNOWN (" . $count . ") : ".str_replace(ABSPATH, '', $location) . ' - ' . strip_tags($message). "</li>";
 			}
 			echo '</ol>';
 		}
